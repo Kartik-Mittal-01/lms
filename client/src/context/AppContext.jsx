d31e476cfb69c84503ef607614a534de14d805ca
Kartik Mittal <kartikmittal8998@gmail.com>
2025-09-26 22:51:18 +0530

    added new apis route

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from 'humanize-duration';
+import {useAuth , useUser}  from '@clerk/clerk-react';
 
 export const AppContext = createContext()
 
@@ -12,6 +13,9 @@ export const AppContextProvider = (props) => {
     const currency = import.meta.env.VITE_CURRENCY
 
     const navigate = useNavigate();
+
+    const {getToken} = useAuth(); 
+    const {user} = useUser();
 
     const [allCourses, setallCourses] = useState([]);
     const [isEducator, setisEducator] = useState(true);
@@ -72,6 +76,17 @@ export const AppContextProvider = (props) => {
       fetchAllCourses();
       fetchUserEnrolledCourses();
     }, [])
+
+    const logToken = async () => {
+        console.log(await getToken());
+    }
+
+    useEffect(() => {
+        if(user){
+            logToken()
+        }
+    }, [user])
+    
     
 
     const value = {
--- server/server.js
+++ server/server.js
@@ -3,23 +3,27 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
 import { clerkWebhooks } from './controllers/webhooks.js';
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
+import connectCloudinary from './configs/cloudinary.js';
+import courseRouter from './routes/courseRoute.js';
 
 // initialise express
 const app = express();
 
 // connect db 
 await connectDB();
+await connectCloudinary();
 
 // middlewares 
 app.use(cors());
+app.use(clerkMiddleware())
 
 // routers
-app.get('/', (req , res) => {
-    res.send("api working");
-})
-
+app.get('/', (req , res) => {res.send("api working");})
 app.post('/clerk' , express.json() , clerkWebhooks )
-
+app.use('/api/educator', express.json() , educatorRouter)
+app.use('/api/course' , express.json() , courseRouter)
 // port 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT , () => {
